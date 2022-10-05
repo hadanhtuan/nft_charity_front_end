d5638490b4448265df5f15289f234ea4de265432
hadanhtuan <20522109@gm.uit.edu.vn>
2022-10-05 13:48:33 +0700

    first pust

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Container } from "@material-ui/core";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 
-function App() {
+import admin_home from "./pages/admin/admin_home";
+
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/admin" exact component={admin_home} />
+      </Switch>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
